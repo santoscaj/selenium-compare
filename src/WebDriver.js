2bfe2430f725faade6c670de936fc90fd282eb81
Albert San <santosc.aj@gmail.com>
2023-05-27 02:20:12 -0400

    restructuring and adding Webdriver class

---
+++ src/WebDriver.js
@@ -0,0 +1,81 @@
+const webdriver = require('selenium-webdriver');
+const fs = require('fs')
+
+const getFileDate = () => new Date().toISOString().replace(/[\-:]/g, '').replace('T', "_").split('.')[0]
+class WebDriver {
+
+    constructor({ name, fileinfo, server, filename, encoding, outdir, debug = false }) {
+        if (filename && fileinfo) throw new Error('Cannot set both filename and fileinfo')
+
+        this.name = name || (Math.random() + 1).toString(36).substring(7).toUpperCase()
+        this.encoding = encoding || 'base64'
+        this.server = server || 'http://localhost:4444/wd/hub'
+
+        // Files are expected to be saved in the format prefix_YYYMMMDD_Website/name_element_suffix.png
+        this.file = filename || fileinfo || { base: getFileDate() }
+        this.outdir = outdir || 'output'
+        this.debug = debug
+    }
+
+    checkProperties(propsToCheck) {
+        if (!this.driver) throw new Error('Driver has not been set up')
+        let error = Object.entries(propsToCheck).find((entry) => !entry[1])
+        if (error) {
+            console.log(propsToCheck)
+            throw new Error(`Missing required property ${error[0]}`)
+        }
+    }
+
+    async setup() {
+        this.driver = new webdriver.Builder()
+            .forBrowser(webdriver.Browser.CHROME)
+            .usingServer(this.server)
+            .build();
+        return this
+    }
+
+    updateFile(fileinfo) {
+        if (fileinfo) this.file = fileinfo
+        else if (typeof this.file === 'object') {
+            this.file.base = getFileDate()
+        }
+        if (this.debug) console.log(`updating file, new file info:  ${this.file}`)
+    }
+
+    saveFile(data, fileinfo = this.file) {
+        let filename = ""
+        if (typeof fileinfo === 'string') {
+            filename = fileinfo
+        } else {
+            filename += this.outdir + '/'
+            if (this?.file?.prefix) filename += this.file.prefix + '_'
+            if (this?.file?.base) filename += this.file.base + '_'
+            filename += this.name
+            if (this?.file?.suffix) filename += this.file.suffix + '_'
+            filename += '.png'
+        }
+        if (this.debug) console.log(`Saving file ${filename}`)
+        fs.writeFileSync(filename, data, this.encoding);
+    }
+
+    async takeScreenshot({ url, screen, element }) {
+        if (this.debug) console.log(`screenshot for url ${url}, details: `, { screen, element })
+        this.checkProperties({ url })
+        await this.driver.get(url);
+        await this.driver
+            .takeScreenshot()
+            .then((image) => {
+                this.saveFile(image)
+            })
+        return this
+    }
+
+    compare(saveProps, ...webdrivers) { }
+
+    close() {
+        this?.driver?.quit()
+    }
+
+}
+
+module.exports = WebDriver
--- src/app.js
+++ src/app.js
@@ -1,26 +1,14 @@
-const webdriver = require('selenium-webdriver');
-const fs = require('fs')
+
 const file_date = new Date().toISOString().replace(/[\-:]/g, '').replace('T', "_").split('.')[0]
 const file = `output/${file_date}.png`
-const encoding_system = 'base64'
-
+const WebDriver = require('./WebDriver')
 const config = require('../config.json')
 
-async function takeScrenshot() {
-
-    let driver = new webdriver.Builder()
-        .forBrowser(webdriver.Browser.CHROME)
-        .usingServer('http://localhost:4444/wd/hub')
-        .build();
-
-    await driver.get('https://selenium.dev');
-
-    driver.takeScreenshot()
-        .then((image) => {
-            fs.writeFileSync(file, image, encoding_system);
-        });
-
-    await driver.quit();
+async function main() {
+    let driver = new WebDriver('mytest')
+    await driver.setup()
+    await driver.takeScreenshot({ url: 'http://google.com' })
+    await driver.close()
 }
 
-// takeScrenshot()
+main()
